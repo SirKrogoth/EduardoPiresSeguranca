41b887309e07b54a2eff7d16c52d474d3ae72a95
João Rafael Fagundes Menezes <menezes.jrafael@gmail.com>
2020-05-21 13:21:24 -0300

    Adicionar arquivos de projeto.

---
+++ AspNetCoreIdentity/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreIdentity.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AspNetCoreIdentity.Controllers
+{
+    [Authorize] //-> Faz com que toda a controller necessite de autenticação para ser acessada.
+    public class HomeController : Controller
+    {
+        [AllowAnonymous] //-> Permite sem que haja uma autenticação
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[Authorize]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "PodeExcluir")]
+        public IActionResult SecretCleam()
+        {
+            return View("Secret");
+        }
+
+        [Authorize(Policy = "PodeEscrever")]
+        public IActionResult SecretCleamGravar()
+        {
+            return View("SecretGravar");
+        }
+
+        [ClaimAuthorize("Home", "Secret")]
+        public IActionResult ClaimCustom()
+        {
+            return View("SecretGravar");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AspNetCoreIdentity/Extensions/RequisitoClaimFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace AspNetCoreIdentity.Extensions
+{
+    public class RequisitoClaimFilter : IAuthorizationFilter
+    {
+        private readonly Claim _claim;
+
+        public RequisitoClaimFilter(Claim claim)
+        {
+            _claim = claim;
+        }        
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if(!CustomAuthorization.ValidarClaimUsuario(context.HttpContext, _claim.Type, _claim.Value))
+            {
+                context.Result = new ForbidResult();
+            }
+        }
+    }
+}
